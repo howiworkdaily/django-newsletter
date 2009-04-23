7eea12f7977e5591cab6aeef41c3bab450fa2648
Kevin Fricovsky <kfricovsky@broadway.com>
2009-04-23 12:09:50 -0400

    changes to setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,24 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
  
-setup(name='django-newsletter',
-      version='0.3',
-      description='A basic, reusable newsletter subscription (opt-in/out) application.',
-      author='Kevin Fricovsky',
-      author_email='kevin@montylounge.com',
-      url='http://github.com/howiworkdaily/django-newsletter/tree/master',
-      packages=['newsletter'],
+setup(
+    name='django-newsletter',
+    version='0.3.0',
+    description='A basic, reusable newsletter subscription (opt-in/out) application.',
+    author='Kevin Fricovsky',
+    author_email='kfricovsky@gmail.com',
+    url='http://github.com/howiworkdaily/django-newsletter/tree/master',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['setuptools'],
 )
+
